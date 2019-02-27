53ee56da02d05540232d269bedaf0f9920c1e535
Tom Fraser <thomas.abercrombie.fraser@gmail.com>
2019-02-27 18:15:13 +0000

    Update db_FORTRAN.f95

--- source/db_FORTRAN.f95
+++ source/db_FORTRAN.f95
@@ -1,4 +1,4 @@
-! db_FORTRAN.f95
+ db_FORTRAN.f95
 
 ! Copyright 2019 Tom Fraser <thomas.abercrombie.fraser@gmail.com>
 
@@ -11,7 +11,6 @@
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
-
 ! You should have received a copy of the GNU General Public License
 ! along with this program; if not, write to:
 ! Free Software Foundation, Inc.,
